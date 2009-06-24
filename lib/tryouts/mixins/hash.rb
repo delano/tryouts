2312b0c52b26ad619962403a3d29d6a71a84f740
delano <delano@solutious.com>
2009-06-24 15:20:55 -0400

    Tidied tryouts

--- lib/tryouts/mixins/hash.rb
+++ lib/tryouts/mixins/hash.rb
@@ -22,10 +22,25 @@ class Hash
   end
   
   unless method_defined?(:last)
+    # Ruby 1.9 doesn't have a Hash#last (but Tryouts::OrderedHash does). 
+    # It's used in Tryouts to return the most recently added instance of
+    # Tryouts to @@instances. 
+    #
+    # NOTE: This method is defined only when Hash.method_defined?(:last)
+    # returns false. 
     def last
       self[ self.keys.last ]
     end
   end
   
+  
+  def gitash(s)
+    # http://stackoverflow.com/questions/552659/assigning-git-sha1s-without-git
+    # http://github.com/mojombo/grit/blob/master/lib/grit/git-ruby/git_object.rb#L81
+    # http://github.com/mojombo/grit/blob/master/lib/grit/git-ruby/git_object.rb#L225
+    # http://www.kernel.org/pub/software/scm/git-core/docs/git-hash-object.html
+    # $ git hash-object file
+    DIGEST_TYPE.hexdigest(("%s %d\0" % ['blob', s.length]) << s)
+  end
 end
 
