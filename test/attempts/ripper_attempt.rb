0f8cc964a7cda4cb7521c1869caa9b218da9093e
delano <delano@solutious.com>
2009-12-01 13:23:54 -0500

    Added YARD attempt

--- test/attempts/ripper_attempt.rb
+++ test/attempts/ripper_attempt.rb
@@ -7,19 +7,30 @@ puts "LEX"
 pp Ripper.lex(sample)
 
 puts "SEXP"
-pp Ripper.sexp(sample)
-
-
+a = Ripper.sexp(sample)
+pp a
 __END__
+:a + :a
+class Test
+ def test_foo
+   assert_equal(2) {
+     1 + 1 if true
+   }
+ end
+end
+
 # test expectation type matters
-'foo' + 'bar'
+#'foo' + 'bar'
+#Test.box('heya') do
+#  1 / 2
+#end
 #=> 'foobar'
 #=> 'foobar'
 
 # test expectation type matters
-foo = 1 + 1
-@bar = 2 + 2
-foo
+#foo = 1 + 1
+#@bar = 2 + 2
+#foo
 #=> 2
 
 
---
+++ test/attempts/yard_attempt.rb
@@ -0,0 +1,50 @@
+require 'yard'
+include YARD::Parser::Ruby::Legacy
+sample = DATA.read
+statements = StatementList.new(sample)
+
+lines = []
+def parse (token, st, expct)
+  if RubyToken::TkBlockContents === token
+    llines, lexpct = [], []
+    st.block.each do |t|
+      parse t, st, lexpct
+    end
+    p [:tests, lexpct]
+  elsif RubyToken::TkCOMMENT === token
+    expct << token.text 
+  else
+    print "#{token.text}"
+  end
+end
+
+statements.each do |st|  # YARD::Parser::Ruby::Legacy::Statement
+  expct = []
+  st.tokens.each_with_index do |token, index|
+    parse token, st, expct
+  end
+  puts
+end
+
+#a.each do |st|
+#  src = st.tokens.map do |token|
+#    YARD::Parser::Ruby::Legacy::RubyToken::TkBlockContents === token ? st.block.to_s : token.text
+#    p [:a, s]
+#    s
+#  end.join
+#  puts src
+#end
+#
+#b = YARD::Parser::Ruby::Legacy::RubyLex.new(sample)
+#p b.methods.sort
+#p YARD::Parser::SourceParser.parse_string(sample)
+
+__END__
+1 + 1
+class Test
+ def test_foo
+   1 + 1 if true
+   #=> 2
+   #=> <Fixnum>
+ end
+end
