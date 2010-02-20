a771db0ca8d6c0eb99e440895786ad9098ae57b5
delano <delano@solutious.com>
2010-02-20 13:01:31 -0500

    Moved mixins/hash to mixins.rb

--- lib/tryouts/mixins.rb
+++ lib/tryouts/mixins.rb
@@ -1,2 +1,37 @@
 
-require "tryouts/mixins/hash"
+class Hash
+  
+  # A depth-first look to find the deepest point in the Hash. 
+  # The top level Hash is counted in the total so the final
+  # number is the depth of its children + 1. An example:
+  # 
+  #     ahash = { :level1 => { :level2 => {} } }
+  #     ahash.deepest_point  # => 3
+  #
+  def deepest_point(h=self, steps=0)
+    if h.is_a?(Hash)
+      steps += 1
+      h.each_pair do |n,possible_h|
+        ret = deepest_point(possible_h, steps)
+        steps = ret if steps < ret
+      end
+    else
+      return 0
+    end
+    steps
+  end
+  
+  unless method_defined?(:last)
+    # Ruby 1.9 doesn't have a Hash#last (but Tryouts::OrderedHash does). 
+    # It's used in Tryouts to return the most recently added instance of
+    # Tryouts to @@instances. 
+    #
+    # NOTE: This method is defined only when Hash.method_defined?(:last)
+    # returns false. 
+    def last
+      self[ self.keys.last ]
+    end
+  end
+  
+end
+
