f9369c997c687230a5d8cd03f9b32eb5e3daece1
delano <delano@cpan.org>
2024-06-20 09:58:34 -0700

    [#10] Cleanup tryouts
    Copilot suggestions for this commit message:
    
    ...for the 2019-2020 season will be held on Sunday, August 25th at 1:00pm at the 10th Street Fields in New Richmond. Please arrive 15 minutes early to check in. If you are unable to attend the tryout, please contact Coach Kelsey at...
    
    Site Links
    
    - Our Staff
    - Locations
    - NRYS Facebook (?)

--- try/step3_try.rb
+++ try/step3_try.rb
@@ -1,19 +1,10 @@
+# frozen_string_literal: true
 
-
-begin
-  raise
-rescue
-  'foo'
-end
-#=> 'foo'
 
 # test handles multiple code lines
 # only only tests last line against expectation
-str = ""
+str = ''
 str << 'foo'
 str << 'bar'
 str
 #=> 'foobar'
-
-
-
