8f730c4ae697a1f77130614d7788214ef5f99f9d
delano <delano@onetimesecret.com>
2025-07-02 23:54:46 -0700

    [#20250701] Remove 3.2 from CI

--- try/step3_try.rb
+++ try/step3_try.rb
@@ -1,7 +1,7 @@
 # try/step3_try.rb
 
 # test handles multiple code lines
-# only only tests last line against expectation
+# only  tests last line against expectation
 str = ''
 str << 'foo'
 str << 'bar'
