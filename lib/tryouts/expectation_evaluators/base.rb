fe9ab73aac4f72001405041c6c1e693de3e71ff0
delano <delano@onetimesecret.com>
2025-07-20 11:01:45 -0700

    Add support for _ for expectation result
    Follows Ruby's idiomatic use of _ while keeping the explicit
    `result` variable for readability.

--- lib/tryouts/expectation_evaluators/base.rb
+++ lib/tryouts/expectation_evaluators/base.rb
@@ -2,32 +2,51 @@
 
 class Tryouts
   module ExpectationEvaluators
+    # Base class for all expectation evaluators
     class Base
       attr_reader :expectation, :test_case, :context
 
+      # @param expectation_type [Symbol] the type of expectation to check
+      # @return [Boolean] whether this evaluator can handle the given expectation type
       def self.handles?(expectation_type)
         raise NotImplementedError, "#{self} must implement handles? class method"
       end
 
+      # @param expectation [Object] the expectation object containing content and metadata
+      # @param test_case [Object] the test case being evaluated
+      # @param context [Object] the context in which to evaluate expectations
       def initialize(expectation, test_case, context)
         @expectation = expectation
         @test_case   = test_case
         @context     = context
       end
 
+      # Evaluates the expectation against the actual result
+      # @param actual_result [Object] the result to evaluate against the expectation
+      # @return [Hash] evaluation result with passed status and details
       def evaluate(actual_result = nil)
         raise NotImplementedError, "#{self.class} must implement evaluate method"
       end
 
       protected
 
+      # Evaluates expectation content in the test context with predefined variables
+      #
+      # This method defines the variables that expectations can access:
+      # - `result`: contains the actual_result if provided
+      # - `_`: shorthand alias for actual_result if provided
+      #
+      # @param content [String] the expectation code to evaluate
+      # @param actual_result [Object] the result to make available as 'result' and '_' variables
+      # @return [Object] the result of evaluating the content
       def eval_expectation_content(content, actual_result = nil)
         path  = @test_case.path
         range = @test_case.line_range
 
-        # Make actual result available as 'result' variable if provided
+        # Make actual result available as 'result' and '_' variables if provided
         if actual_result
           @context.define_singleton_method(:result) { actual_result }
+          @context.define_singleton_method(:_) { actual_result }
         end
 
         @context.instance_eval(content, path, range.first + 1)
