e24b0203e7c95b5056a516384f9cc013441e9696
delano <delano@onetimesecret.com>
2025-07-21 08:35:53 -0700

    Add separate safety tests for setup, teardown

---
+++ try/core/test_batch_setup_safety_try.rb
@@ -0,0 +1,17 @@
+# try/core/test_batch_setup_safety_try.rb
+#
+# Tests for extenuating circumstances
+
+require_relative '../../lib/tryouts'
+
+test_file = 'foo/bar/setup_baz_try.rb'
+Tryouts::PrismParser.new(test_file)
+
+## TEST: We never get to this testcase b/c setup fails
+raise SystemExit
+#=<> nil
+
+puts <<MESSAGE
+This teardown is never reached. If you can see this message it means that
+even though the setup failed tryouts continued to run the batch erroneously.
+MESSAGE
---
+++ try/core/test_batch_teardown_safety_try.rb
@@ -0,0 +1,18 @@
+# try/core/test_batch_teardown_safety_try.rb
+#
+# Tests for extenuating circumstances
+
+require_relative '../../lib/tryouts'
+
+puts <<MESSAGE
+We should see this setup message and the testcase in this file should pass.
+The teardown should fail because we intentionally caused an error by
+reading a non-existent file.
+MESSAGE
+
+## TEST: A basic test
+1 + 1
+#=> 2
+
+test_file = 'foo/bar/setup_baz_try.rb'
+Tryouts::PrismParser.new(test_file)
